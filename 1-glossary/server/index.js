c5c607f5b8fa6bea03980f20a5f548fa2761a37e
Johnathan Simeroth <john.simeroth@gmail.com>
2023-03-08 11:23:43 -0800

    added app, updated process.env Port in server, rendering hello world  in browser

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+process.env.PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
 app.listen(process.env.PORT);
